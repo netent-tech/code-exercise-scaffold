640f189d089cd532791cf78412e6a85c32f1e97e
Jens Lundberg <jens.lundberg@netent.com>
2020-01-17 14:05:11 +0100

    initial commit

---
+++ src/test/java/codeexercise/DummyTest.java
@@ -0,0 +1,13 @@
+package codeexercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DummyTest {
+
+    @Test
+    void doNothing() {
+        assertTrue(true);
+    }
+}
