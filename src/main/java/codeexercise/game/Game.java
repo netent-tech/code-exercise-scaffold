bf853b20334a13ea9d8d58490537cb1370386eb8
Niclas Berggren <niclas.berggren@netent.com>
2020-03-10 16:05:43 +0100

    Implement deploy and list games

--- src/main/java/codeexercise/game/Game.java
+++ src/main/java/codeexercise/game/Game.java
@@ -1,9 +1,33 @@
 package codeexercise.game;
 
+import java.util.Objects;
+
 public class Game {
 
-    public String getId() {
-        return null;
+    private final String id;
+
+    public Game(String id) {
+        this.id = id;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/test/java/codeexercise/gameservice/GameServiceTest.java
+++ src/test/java/codeexercise/gameservice/GameServiceTest.java
@@ -1,14 +1,22 @@
 package codeexercise.gameservice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
+import codeexercise.game.Game;
 import org.junit.jupiter.api.Test;
 
 class GameServiceTest {
 
+    private GameService subject = new GameService();
+
     @Test
-    void deploysGame() {
-        assertTrue(true);
+    void gameIsDeployed() {
+        String expectedId = "a-game";
+
+        subject.deploy(new Game(expectedId));
+
+        assertThat(subject.listGamesIds(), containsInAnyOrder(expectedId));
     }
 
 }
